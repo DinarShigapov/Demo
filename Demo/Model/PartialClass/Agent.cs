b38248aec7e2d73ba1c268672d2115ced78e905d
FLAS SON <dinar-shigapov@inbox.ru>
2023-04-24 16:42:22 +0300

    Добавьте файлы проекта.

---
+++ Demo/Model/PartialClass/Agent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    public partial class Agent
+    {
+        public string FullName
+        {
+            get 
+            {
+                return $"{AgentType.Title} | {Title}";
+            }
+        }
+    }
+}
